998aac9a75592e7344cee8e792b985b7cea1e0c3
Adam Vitti <advi4972@colorado.edu>
2021-03-12 12:52:03 -0700

    Added button 1.1 configuration

--- button.c
+++ button.c
@@ -13,6 +13,18 @@ void config_button(void)
     //config S1 on the MSP432P401R SimpleLink Launchpad as a button interrupt
     //set up Button Interrupt
 
+    P1->DIR &= ~BIT1; //sets p1.1 as input
+    P1->REN |= BIT1; //uses pullup resistor configuration
+    P1->OUT |= BIT1;
+
+    P1->SEL0 &= ~BIT1; //0000000000 GENERAL IO PIN CONFIG
+    P1->SEL1 &= ~BIT1;// 0000000000
+    P1->DIR &= ~BIT1; //sets p1.1 as input
+
+    P1->REN |= BIT1; //uses pullup resistor config
+    P1->IES |= BIT1; //Falling edge interrupt trigger
+    P2->IFG &= ~BIT1; //Clears initial interrupt flag
+    P2->IE |= BIT1; //Enables interrupt functionality for P1.1
 }
 
 /* Configure NVIC for Interrupt Source */
